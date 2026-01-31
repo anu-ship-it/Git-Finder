3f0c9c93b09f3e32da9f317d4e8f4c526ba490eb
anu-ship-it <anup17508@gmail.com>
2026-01-31 11:39:23 +0530

    Need more changes in this file

--- eslint.config.js
+++ eslint.config.js
@@ -5,5 +5,9 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
-    
+    { ignores: ['dist'] },
+    {
+        files: ['**/*.{js,jsx}'],
+        
+    }
 ]
