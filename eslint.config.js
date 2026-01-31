51cf800e5d6b92bc970a16a60d7e5338d71d9850
anu-ship-it <anup17508@gmail.com>
2026-01-31 11:47:27 +0530

    Few more changes are coming

--- eslint.config.js
+++ eslint.config.js
@@ -26,6 +26,10 @@ export default [
         },
         rules: {
             ...js.configs.recommended.rules,
+            ...react.configs.recommended.rules,
+            ...react.configs['jsx-runtime'].rules,
+            ...reactHooks.configs.recommended.rules,
+            ''
         }
     }
 ]
