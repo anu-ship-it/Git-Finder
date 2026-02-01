f66ecdada9bddc8b19f59a534987becb49f27c42
Anoop Kumar <anup17508@gmail.com>
2026-02-01 22:51:00 +0530

    Few more changes need

--- src/App.jsx
+++ src/App.jsx
@@ -8,3 +8,17 @@ import useGitHubApi from './hooks/useGitHubApi';
 import { FaGithub, FaTwitter, FaCoffee, FaMoon, FaSun, FaSearch, FaUsers, FaCode } from 'react-icons/fa';
 import LoadingSpinner from './components/LoadingSpinner';
 
+function App() {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [darkMode, setDarkMode] = useState(() => {
+        return localStorage.getItem('darkMode') === 'true';
+    });
+    const { userData, buddies, languages, activities, loading, buddiesLoading, error } = useGitHubApi(searchTerm);
+
+    useEffect(() => {
+        document.body.classList.toggle('dark-mode', darkMode),
+        localStorage.setItem('darkMode', darkMode);
+    }, [darkMode]);
+
+    
+}
