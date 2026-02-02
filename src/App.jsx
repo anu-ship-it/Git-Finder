58875e2f4d745abc201ff8252337531a97b199fa
Anoop Kumar <anup17508@gmail.com>
2026-02-02 21:50:45 +0530

    Some more changes have been done

--- src/App.jsx
+++ src/App.jsx
@@ -65,7 +65,9 @@ function App() {
                         <p>Visualize laguage perferences and coding activity patterns</p>
                     </div>
                 </div>
+            </div>
         )}
-    </div>
+
+        
    )
 }
