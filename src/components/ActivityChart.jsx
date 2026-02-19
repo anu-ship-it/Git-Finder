f4ef38328123660b5dceb53d660bdcaa370769e3
Anoop Kumar <anup17508@gmail.com>
2026-02-19 10:21:00 +0530

    Changes done in this file

--- src/components/ActivityChart.jsx
+++ src/components/ActivityChart.jsx
@@ -2,23 +2,23 @@ import React from 'react';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 function ActivityChart({ activities }) {
-    const data = Object.entries(activities).map(([name, value]) => ([ name, value ]));
+  const data = Object.entries(activities).map(([name, value]) => ({ name, value }));
 
-    return (
-        <div className="activity-chart">
-            <h3>Recent Activity</h3>
-            <ResponsiveContainer width="100%" height={300}>
-                <BarChart data={data}>
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis datakey="name" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
-                    <Bar datakey="value" fill="#8884d8" />
-                </BarChart>
-            </ResponsiveContainer>
-        </div>
-    );
+  return (
+    <div className="activity-chart">
+      <h3>Recent Activity</h3>
+      <ResponsiveContainer width="100%" height={300}>
+        <BarChart data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="value" fill="#8884d8" />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
 }
 
 export default ActivityChart;
