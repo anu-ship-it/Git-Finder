6cf77c695414f14c3cd9f4d5968cbc9b295d2cd1
Anoop Kumar <anup17508@gmail.com>
2026-02-20 20:44:18 +0530

    Few more changes needed

--- src/components/BuddyList.jsx
+++ src/components/BuddyList.jsx
@@ -1,3 +1,21 @@
 import React from "react";
 import { FaSpinner } from "react-icons/fa";
 
+function BuddyList({ buddies, loading }) {
+    const handleBuddyClick = (buddy) => {
+        window.open(`https://github.com/${buddy.login}`, `_blank`);
+    };
+
+    if (loading) {
+        return (
+            <div className="buddy-list-loading">
+                <FaSpinner className="spinner" />
+                <p>Finding your true buddies... This may take a few moments💖</p>
+            </div>
+        );
+    }
+
+    return (
+        
+    )
+}
