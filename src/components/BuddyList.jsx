5a043f66f8295066a4343fe3da1787fc1bee3d28
Anoop Kumar <anup17508@gmail.com>
2026-02-21 17:51:51 +0530

    Changes made

--- src/components/BuddyList.jsx
+++ src/components/BuddyList.jsx
@@ -24,11 +24,15 @@ function BuddyList({ buddies, loading }) {
                         <img src={buddy.avatar_url} alt={buddy.login} />
                         <div className="buddy-info">
                             <span className="buddy-name">{buddy.login}</span>
-                            
+                            <span className="match-score">Overall Match: {buddy.matchScore.toFixed(2)}%</span>
+                            <span className="compatibility">Language Compatibility: {buddy.languageCompatibility.toFixed(2)}%</span>
+                            <span className="compatibility">Activity Compatibility: {buddy.activityCompatibility.toFixed(2)}%</span>
                         </div>
                     </li>
                 ))}
             </ul>
         </div>
-    )
+    );
 }
+
+export default BuddyList;
