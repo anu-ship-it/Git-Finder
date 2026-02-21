8fb2ade0f73e615aa18d379781b80777f738060a
Anoop Kumar <anup17508@gmail.com>
2026-02-21 18:02:34 +0530

    More changes might come

--- src/components/LanguageChart.jsx
+++ src/components/LanguageChart.jsx
@@ -4,4 +4,6 @@ import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer } from 'recharts';
 
 const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#8884D8'];
 
-function
+function LanguageChart({ languages }) {
+    if
+}
