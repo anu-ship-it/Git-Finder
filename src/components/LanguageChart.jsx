6937aa6b00a53b91b1159beba596e12373ccc201
Anoop Kumar <anup17508@gmail.com>
2026-02-21 17:56:38 +0530

    This is important file

---
+++ src/components/LanguageChart.jsx
@@ -0,0 +1,7 @@
+// src/components/LanguageChart.jsx
+import React from "react";
+import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer } from 'recharts';
+
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#8884D8'];
+
+function
