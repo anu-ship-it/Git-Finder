0ba4852308822c2382eb592758f870239c729e74
Anoop Kumar <anup17508@gmail.com>
2026-02-21 18:06:53 +0530

    Need more changes

--- src/components/LanguageChart.jsx
+++ src/components/LanguageChart.jsx
@@ -5,5 +5,16 @@ import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer } from 'recharts';
 const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#8884D8'];
 
 function LanguageChart({ languages }) {
-    if
+    if (!languages || Object.keys(languages).length === 0) {
+        return <div className="language-chart">No language data available</div>;
+    }
+
+    const data = Object.entries(languages).map(([name, value]) => ({ name, value }));
+
+    return (
+        <div className="language-chart">
+            <h3>Top Languages</h3>
+            
+        </div>
+    )
 }
