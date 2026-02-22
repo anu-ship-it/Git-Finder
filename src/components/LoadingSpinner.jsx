9745c5062994dd8a4e07e2b09acd993f644c0a0e
Anoop Kumar <anup17508@gmail.com>
2026-02-22 20:44:54 +0530

    More changes are coming

---
+++ src/components/LoadingSpinner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function LoadingSpinner() {
+    return (
+        <div className="loading-spinner" role="status">
+            <div className="spinner"></div>
+            <span className="sr-only">Loading...</span>
+        </div>
+    );
+}
+
+export default LoadingSpinner;
