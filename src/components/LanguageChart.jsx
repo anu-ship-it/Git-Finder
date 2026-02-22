46e87bb101115370b86b580a677650ae910e1bbf
Anoop Kumar <anup17508@gmail.com>
2026-02-22 20:32:58 +0530

    Need some corrections

--- src/components/LanguageChart.jsx
+++ src/components/LanguageChart.jsx
@@ -18,8 +18,16 @@ function LanguageChart({ languages }) {
                 <PieChart>
                     <Pie
                       data={data}
+                      cx="50"
+                      cy="50%"
+                      labelLine={false}
+                      outerRadius={80}
+                      fill="#8884d8"
+                      datakey="value"
                     >
-
+                      {data.map((entry, index) => (
+                        <Cell key={`cell-${index}` fill={COLORS[index % COLORS.length]}} />
+                      ))}
                     </Pie>
                 </PieChart>
             </ResponsiveContainer>
